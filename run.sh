87e18d2d5956542f1283325cf85695e8997a9cfa
Val Huber <valjhuber@gmail.com>
2022-10-12 14:59:08 -0700

    no use of source

--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #! /bin/bash
 
-alias activate=". venv/bin/activate"
-
-fenv(){
-    venv/bin/activate.csh
-}
-# activate
-# source venv/bin/activate
-# activate fails - source not found
-
-# source venv/bin/activate; pip freeze
-# alias activate="source venv/bin/activate"; activate; pip freeze
-
 activate; python3 -m pip freeze
 
 . venv/bin/activate; python3 -m pip freeze
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ read -p "Press [Enter] create small test venv > "
 
 set +x
 
-python3 -m venv venv; source venv/bin/activate; python3 -m pip install python-dotenv
+python3 -m venv venv; . venv/bin/activate; python3 -m pip install python-dotenv
 
 echo "Now: run sh run.sh"
 echo "  .. verify it contains (only) python-dotenv"
